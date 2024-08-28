713bbc587b4fc60ea0276e6a89c9220dab2416cc
Angel Hernandez <angeldariaux@gmail.com>
2024-08-27 20:41:49 -0600

    agregadas las clases para los asientos, los elementos reservables, y la interfaz para los asientos reservables

--- seatReservation/Program.cs
+++ seatReservation/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Definicion del Enum para el estado del asiento
+
+public enum EstadoAsiento
+{
+  Disponible, 
+  Reservado,
+  Ocupado
+}
+
+// Interfaz para asientos reservables 
+
+public interface IReservable  // porque tiene una I adelante? 
+{
+  void Reservar();
+  void CancelarReserva();
+}
+
+// Clase Abstracta para Elementos Reservables 
+
+public abstract class ElementoReservable 
+{
+  public string Identificador {get; set;}
+  public EstadoAsiento Estado {get; set;}
+
+  public ElementoReservable(string identificador)
+  {
+    Identificador = identificador;
+    Estado = EstadoAsiento.Disponible;
+  }
+
+  public abstract void MostrarEstado();
+}
+
+// Clase Concreta para asiento
+
+public class Asiento : ElementoReservable, IReservable
+{
+  public Asiento(string numeroAsiento) : base(numeroAsiento)
+  {
+
+  }
+
+  public void Reservar()
+  {
+    if (Estado == EstadoAsiento.Disponible)
+    {
+      Estado = EstadoAsiento.Reservado;
+      Console.WriteLine($"El asiento {Identificador} ha sido reservado.");
+    }
+    else 
+    {
+      Console.WriteLine($"El asiento {Identificador} no esta disponible");
+    }
+  }
+
+  public void CancelarReserva()
+  {
+    if (Estado == EstadoAsiento.Reservado)
+    {
+      Estado = EstadoAsiento.Disponible;
+      Console.WriteLine($"La reservacion del asiento {Identificador} ha sido cancelada.");
+    }
+    else 
+    {
+      Console.WriteLine($"El asiento {Identificador} no estaba reservado.");
+    }
+  }
+
+    public override void MostrarEstado()
+    {
+        Console.WriteLine($"Asiento {Identificador}: {Estado}");
+    }
+}
+
+// Clase para la sala de cine
+
